bdba8c1160cdaa5622d64664aa596c8c465f799f
YazdanPlug <73750616+YazdanPlug@users.noreply.github.com>
2021-02-11 20:25:33 +0330

    Update BedWars.java

--- src/main/iR.yzplug.BW/BedWars.java
+++ src/main/iR.yzplug.BW/BedWars.java
@@ -1,1 +1,13 @@
+package ir.yzn.bw;
 
+import ir.yzn.bw.EvEnTs;
+import cn.nukkit.plugin.PluginBase;
+
+public class BedWars extends PluginBase {
+  @Override
+  public void onEnable(){
+    this.getLogger().info("Enabled BedWars plugin on Nukkit by YazdanPlug");
+    PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new EvEnTs(), this);
+  }
+}
