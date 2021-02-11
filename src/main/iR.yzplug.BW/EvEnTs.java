f8ae17c59f70ab6d950dcc9b2a0dc86d81a6bec7
YazdanPlug <73750616+YazdanPlug@users.noreply.github.com>
2021-02-11 20:38:12 +0330

    Update EvEnTs.java

--- src/main/iR.yzplug.BW/EvEnTs.java
+++ src/main/iR.yzplug.BW/EvEnTs.java
@@ -3,5 +3,15 @@ package ir.yzn.bw;
 import cn.nukkit.event.Listener;
 
 public class EvEnTs implements Listener {
-
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+    switch (command.getName().toLowerCase()){
+      case "bw" :
+        sender.sendMessage("++--//{ EggWars-NK }\\--++");
+        sender.sendMessage("join : /ew join");
+        sender.sendMessage("leave : /ew leave");
+        sender.sendMessage("create : /ew create");
+        sender.sendMessage("list : /ew list");
+  }
+    
 }
